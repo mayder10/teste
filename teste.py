8ea7b0a754350d67868beeacf201843c684f7deb
mayder10 <denise.mayder@gmail.com>
2025-08-24 20:18:57 -0300

    acresentei meus robis

--- teste.py
+++ teste.py
@@ -2,7 +2,12 @@ nome = "Denise"
 idade = 30
 profissao = "analista de dados"
 
+meus_hobbies = ["jogar", "viajar", "ler na biblia"]
+duvidas =                
+
 
 print("nome: ", nome)
 print("idade: ", idade)
 print("Profissao: ", profissao)
+print("Hobbies: ", meus_hobbies)
+print("Duvidas: ", duvidas)
