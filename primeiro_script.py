fbcd2983d4241f37d76fb73f5d86519ec7bfc46a
mayder10 <denise.mayder@gmail.com>
2025-11-02 18:58:48 -0300

    melhorias no meu primeiro_script

---
+++ primeiro_script.py
@@ -0,0 +1,11 @@
+nome = "Denise" 
+idade = 30
+profissao = "analista de dados"
+
+meus_hobbies = ["jogar", "viajar", "ler na biblia"]
+duvidas = [15  ]         
+
+print ("ola mundo")
+print(f"Meu nome e : {nome}, eu tenho: {idade}, quero ser: {profissao}, gosto de: {meus_hobbies}, tenho duvidas: {duvidas}")
+
+
